db9c835503e32b27b6d4600bec2da3933fac3b6b
Kai <81066723+JavaScriptpy@users.noreply.github.com>
2022-03-05 20:29:26 +0800

    Update arrest.js

--- v1/commands/moderation/arrest.js
+++ v1/commands/moderation/arrest.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "arrest",
   aliases: ['rashomon'],
-  code: `$channelSendMessage[924580942281068554;{title:Arrest Log}{description:**$userTag** arrested **$userTag[$mentioned[1]]**
+  code: `$channelSendMessage[LOG_CHANNEL1_ID;{title:Arrest Log}{description:**$userTag** arrested **$userTag[$mentioned[1]]**
 
 > **Moderator**
 $userTag ($authorID)
@@ -11,7 +11,8 @@ $userTag[$mentioned[1]] ($mentioned[1])
 $day[yes] $month $day $year - $hour:$minute:$second UTC
 > **Channel Where Command Is Executed**
 $channelName ($channelID)}{color:$getVar[default]}{timestamp}]
-$channelSendMessage[922407637411328010;{title:Arrest Log}{description:**$userTag** arrested **$userTag[$mentioned[1]]**
+
+$channelSendMessage[LOG_CHANNEL2_ID;{title:Arrest Log}{description:**$userTag** arrested **$userTag[$mentioned[1]]**
 
 > **Moderator**
 $userTag ($authorID)
@@ -32,7 +33,7 @@ $title[ARRESTED!]
 $description[**$userTag[$mentioned[1]]** broke a leg. Oops.]
 $image[https://eca.kyle9.repl.co/media/arrest.gif]
 $color[$getVar[default]]
-$giveRole[$mentioned[1];933655133332602880]
+$giveRole[$mentioned[1];ARREST_ROLE]
 $deletecommand
 
 $onlyIf[$rolePosition[$highestRole[$authorID]]<$rolePosition[$highestRole[$mentioned[1]]];{description:You can't arrest someone who is higher than you!}{color:$getVar[red]}]
@@ -40,5 +41,5 @@ $onlyIf[$isBot[$mentioned[1]]!=true;{description:You can't arrest a bot!}{color:
 $onlyIf[$mentioned[1]!=$authorID;{description:You can't arrest yourself!}{color:$getVar[red]}]
 $onlyIf[$mentioned[1]!=;{description:Please mention the user you want to arrest!}{color:$getVar[default]}]
 $argsCheck[1;{title:Usage}{description:\`?arrest <@user>\`}{color:$getVar[red]}]
-$onlyForRoles[920265063087104000;920264976663457792;922403776059559947;{description:You can't use this command!}{color:$getVar[red]}]`
+$onlyForRoles[AUTHORIZED1_ROLEID;AUTHORIZED2_ROLEID;{description:You can't use this command!}{color:$getVar[red]}]`
 }
