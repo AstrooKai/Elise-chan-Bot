48945d4b054f3940c568ef46264432eb3bdd87e2
Kai <81066723+AstrooKai@users.noreply.github.com>
2023-02-11 22:52:37 +0800

    Delete v2 directory

--- v2/commands/fun/bungogif.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "bungogif",
-  aliases: ['bungogifs'],
-  code: `$title[$getObjectProperty[name]]
-$image[$getObjectProperty[link]]
-$color[$getVar[default]]
-$createObject[$jsonRequest[https://eca.astrookai.repl.co/bungogifs]]`
-}
